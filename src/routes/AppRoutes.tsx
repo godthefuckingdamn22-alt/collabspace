40a12f83fb4630de1e097ed43e5fdcbe21a06078
manaloreycon603-jpg <manaloreycon603@gmail.com>
2026-09-24 00:25:58 +0800

    Complete settings page

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -2,16 +2,20 @@ import { Routes, Route } from "react-router-dom";
 import Dashboard from "../pages/Dashboard";
 import Login from "../pages/Login";
 import Projects from "../pages/Projects";
+import Settings from "../pages/Settings";
+import Signup from "../Signup";
 import MainLayout from "../layouts/MainLayout";
 
 function AppRoutes() {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
 
       <Route element={<MainLayout />}>
         <Route path="/" element={<Dashboard />} />
         <Route path="/projects" element={<Projects />} />
+        <Route path="/settings" element={<Settings />} />
       </Route>
     </Routes>
   );
